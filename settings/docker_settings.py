3e1eaa452d95e9fc3950b525548d0c5d82084511
nosanity <nosanity17@gmail.com>
2019-06-06 20:19:57 +0300

    test

--- settings/docker_settings.py
+++ settings/docker_settings.py
@@ -1,10 +1,10 @@
 import os
 
-DB_NAME = os.getenv('DB_NAME', '__DEFINE_ME__')
-DB_USER = os.getenv('DB_USER', '__DEFINE_ME__')
-DB_PASSWORD = os.getenv('DB_PASSWORD', '__DEFINE_ME__')
-DB_HOST = os.getenv('DB_HOST', '__DEFINE_ME__')
-DB_PORT = os.getenv('DB_PORT', '__DEFINE_ME__')
+DB_NAME = os.getenv('DB_NAME', 'db')
+DB_USER = os.getenv('DB_USER', 'test')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1')
+DB_PORT = os.getenv('DB_PORT', '5432')
 
 SSO_URL = os.getenv('SSO_URL', '__DEFINE_ME__')
 SOCIAL_AUTH_UNTI_KEY = os.getenv('SOCIAL_AUTH_UNTI_KEY', '__DEFINE_ME__')
